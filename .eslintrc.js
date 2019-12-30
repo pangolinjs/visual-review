97336663769f33374e3633aaeef330d841c172d2
Fynn Becker <post@fynn.be>
2019-12-30 09:18:17 +0100

    chore: Fix Svelte-ESLint incompatibilities

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
       processor: 'svelte3/svelte3',
       rules: {
         'import/first': 0,
+        'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 2, maxEOF: 0 }],
         'no-undef-init': 0
       }
     }
